b01ebe56e721dd8d578afd105d8333e610f55bac
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2018-06-26 17:33:10 -0300

    Add audio tunnings

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
-export CROSS_COMPILE=~/developement/Kernel_Toolchain/bin/aarch64-linux-android-
+export CROSS_COMPILE=~/developement/Kernel_Toolchain_8/bin/aarch64-linux-android-
 export ARCH=arm64
 make santoni_defconfig
-make -j40
+make -j24
